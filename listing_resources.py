4914116646287aa77ab47aefc8a8babec2dff832
Zaire Ali <zaireali649@gmail.com>
2025-05-01 19:54:28 -0400

    Update listing_resources.py

--- listing_resources.py
+++ listing_resources.py
@@ -1,25 +1,40 @@
+from typing import List, Dict, Any
 from helpers import *
 
-def print_bucket_names(s3_client):
-    bucket_names = list_buckets(s3_client)
-    
-    for bucket_name in bucket_names: # same as "\n".join(bucket_names)
+
+def print_bucket_names(s3_client: Any) -> None:
+    """
+    Prints the names of all S3 buckets returned by the list_buckets helper function.
+
+    Args:
+        s3_client (Any): Boto3 S3 client object or equivalent.
+    """
+    bucket_names: List[str] = list_buckets(s3_client) # same as "\n".join(bucket_names)
+
+    for bucket_name in bucket_names:
         print(bucket_name)
 
-def print_instance_ids(ec2_client):
-    instances = describe_instances(ec2_client)
 
-    instance_ids = []
+def print_instance_ids(ec2_client: Any) -> None:
+    """
+    Prints the instance IDs from a list of EC2 instances returned by the describe_instances helper function.
+
+    Args:
+        ec2_client (Any): Boto3 EC2 client object or equivalent.
+    """
+    instances: List[Dict[str, Any]] = describe_instances(ec2_client)
+
+    instance_ids: List[str] = []
     for instance in instances:
         instance_ids.append(instance['InstanceId'])
 
     for instance_id in instance_ids:
-        print(instance_id) 
+        print(instance_id)
 
 
-if "__main__" == __name__:
-    ec2_client = get_ec2_client()
-    s3_client = get_s3_client()
+if __name__ == "__main__":
+    ec2_client: Any = get_ec2_client()
+    s3_client: Any = get_s3_client()
 
     print_bucket_names(s3_client)
     print_instance_ids(ec2_client)
