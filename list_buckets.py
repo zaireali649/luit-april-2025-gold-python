d32cc67353234bbd8ab82454088ac47c4443f846
Zaire Ali <zaireali649@gmail.com>
2025-05-05 19:49:15 -0400

    Update list_buckets.py

--- list_buckets.py
+++ list_buckets.py
@@ -1,20 +1,15 @@
-#%%
-
-# list S3 Buckets
-
+# Import the Boto3 library to interact with AWS services
 import boto3
 
+# Create an S3 client using Boto3
 s3 = boto3.client('s3')
 
-#%%
-
+# Retrieve a list of all S3 buckets in the account
 response = s3.list_buckets()
 
-#%%
-
+# Extract the list of bucket dictionaries from the response
 buckets = response["Buckets"]
 
-#%%
-
+# Loop through each bucket and print its name
 for bucket in buckets:
     print(bucket["Name"])
